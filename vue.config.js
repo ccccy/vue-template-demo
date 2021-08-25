a5f9ff6e3cafd834d2e26f33f6f7f1636118ab95
yu.cao <605246595@qq.com>
2021-08-25 16:30:38 +0800

    前后端连通

--- vue.config.js
+++ vue.config.js
@@ -31,6 +31,13 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
+    proxy: {
+      '/api/': {
+        target: 'http://127.0.0.1:8888',
+        ws: true,
+        changeOrigin: true
+      }
+    },
     open: true,
     overlay: {
       warnings: false,
